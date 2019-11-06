637ebd16ee9c11eee9c09e0b1379e2b1b59cfdd4
hespanhol <hespanhol@mbp-de-raphael>
2019-11-06 08:50:04 -0200

    Was generated the basic structure of our application

---
+++ src/main/java/com/raphaelhespanhol/eletrostore/EletroStoreApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelhespanhol.eletrostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphaelhespanhol/eletrostore/EletroStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelhespanhol.eletrostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletroStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
